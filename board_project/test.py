bbdd299ce03ee7fb601271c9b7ed737e9fd8f1ed
bitnara-opasnet <84299110+bitnara-opasnet@users.noreply.github.com>
2021-08-17 14:25:18 +0900

    board_0817

---
+++ board_project/test.py
@@ -0,0 +1,39 @@
+import xlsxwriter 
+
+def make_workbook(name, table_col, table_data):
+  workbook = xlsxwriter.Workbook(name)
+  worksheet = workbook.add_worksheet()
+  bold = workbook.add_format({'bold':True})
+  worksheet.write_row(0, 0, table_col, bold) 
+  row = 1
+  for i in table_data:
+    worksheet.write_row(row, 0, i)
+    row += 1
+  workbook.close() 
+
+def make_workbook_chart(name, table_col, table_data, chart_col, chart_data, chart_start_num, chart_end_num, type, chart_title, chart_position):
+  workbook = xlsxwriter.Workbook(name)
+  worksheet = workbook.add_worksheet()
+  bold = workbook.add_format({'bold':True})
+  worksheet.write_row(0, 0, table_col, bold) 
+  row = 1
+  for i in table_data:
+    worksheet.write_row(row, 0, i)
+    row += 1
+
+  worksheet.write_row(chart_start_num, 0, chart_col, bold)
+  row = chart_start_num +1
+  for i in chart_data:
+    worksheet.write_row(row, 0, i)
+    row += 1
+
+  chart1 = workbook.add_chart({'type': type})
+  chart1.add_series({
+    'name': ['sheet1', chart_start_num, 0],
+    'categories' : '=Sheet1!$A${}:$A${}'.format(chart_start_num+2, chart_end_num),
+    'values': '=Sheet1!$B${}:$B${}'.format(chart_start_num+2, chart_end_num)
+    })
+  chart1.set_title({'name': chart_title})
+  worksheet.insert_chart(chart_position, chart1)
+
+  workbook.close()  
---
+++ board_project/views/main_views.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_paginate import Pagination, get_page_parameter
+from models import Board
+import os
+import sqlite3
+import psycopg2
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
+
+basedir = os.path.abspath(os.path.dirname(__file__)) 
+dbfile = os.path.join(basedir, 'board.db') 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
+db.init_app(app) 
+db.app = app 
+db.create_all()  
+
+@app.route('/')
+def index():
+    return "hello world"
+
+# 게시판 내용 조회 (Read)
+@app.route('/') 
+def list():
+    page=request.args.get(get_page_parameter(), type=int, default=1)
+    boards = Board.query.all()
+    boards = Board.query.paginate(page=page, per_page=5)
+    return render_template('list.html', rows=boards) 
+
+# 게시판 내용 추가 (Create)
+@app.route('/add',methods=["POST", "GET"])
+def add():
+    if request.method == "POST":
+        new_board = Board(name=request.form['name'], context=request.form['context'])
+        db.session.add(new_board)
+        db.session.commit()
+        return redirect(url_for("index"))
+    else:  
+        return render_template("input.html")
